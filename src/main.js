aace3f606e5f12270405f7d1d1140322430e9953
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-05-21 11:49:22 -0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
-const body = document.querySelector('body');
-const test = document.getElementById('test');
+const main = document.querySelector('main');
+const header = document.querySelector('header');
 
-test.addEventListener('click', lightenBackground.bind(this));
+document.addEventListener('DOMContentLoaded', glowUp());
 
-function lightenBackground(){
-    body.classList.remove('bg-gray-300');
-    body.classList.add('bg-gray-50');
+function glowUp(){
+    //Dry out wall
+    setTimeout(()=>  main.classList.add('animate-dry'), 2000);
+    setTimeout(()=>  main.classList.add('bg-gray-500'), 7000);
+    setTimeout(()=>  main.classList.remove('bg-gradient-to-b'), 7000);
+    setTimeout(()=>  main.classList.remove('from-zinc-500'), 7000);
+    setTimeout(()=>  main.classList.remove('from-55%'), 7000);
+    setTimeout(()=>  main.classList.remove('to-neutral-600'), 7000);
+    setTimeout(()=>  main.classList.remove('to-60%'), 7000);
+    setTimeout(()=>  main.classList.remove('bg-fixed'), 7000);
+    setTimeout(()=>  main.classList.remove('animate-dry'), 7000);
+    //Paint wall
+    setTimeout(()=>  main.classList.add('animate-lightenWall'), 7000);
+    setTimeout(()=>  main.classList.add('bg-[#97cde6]'), 14000);
+    setTimeout(()=>  main.classList.remove('bg-gray-500'), 14000);
+    setTimeout(()=>  main.classList.remove('animate-lightenWall'), 14000);
+    //Paint Ceiling
+    setTimeout(()=>  header.classList.add('animate-lightenCeiling'), 14000);
+    setTimeout(()=>  header.classList.add('bg-[#ede7df]'), 21000);
+    setTimeout(()=>  header.classList.add('text-[#302920]'), 21000);
+    setTimeout(()=>  header.classList.remove('bg-[#635b50]'), 21000);
+    setTimeout(()=>  header.classList.remove('text-[#ebe6e1]'), 21000);
+    setTimeout(()=>  header.classList.add('animate-addCeilingLight'), 21000);
+    setTimeout(()=>  header.classList.add('shadow-yellow-50'), 23000);
+    setTimeout(()=>  header.classList.remove('shadow-zinc-900'), 23000);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,48 @@ module.exports = {
     extend: {
       fontFamily: {
         'caveat': ["Caveat", ...defaultTheme.fontFamily.serif]    
+      },
+      'animation': {
+        'dry':'dry 10s 1s 1 ease',
+        'lightenWall':'lightenWall 7s 0s 1 ease',
+        'lightenCeiling':'lightenCeiling 10s 0s 1 ease',
+        'addCeilingLight':'addCeilingLight 2s 0s 1 ease'
+    },
+    'keyframes': {
+        'dry': {
+            '0%': {
+                'background-size':'100% 100%'
+            },
+            '100%': {
+                'background-size':'200% 200%'
+            }
+        },
+        'lightenWall': {
+          '0%': {
+            'background': '#71717a'
+          },
+          '100%': {
+            'background': '#97cde6'
+          }
+        },
+        'lightenCeiling': {
+          '0%': {
+            'background': '#635b50',
+            'color': '#ebe6e1'
+          },
+          '100%': {
+            'background': '#ede7df',
+            'color': '#302920'
+          },
+          'addCeilingLight': {
+            '0%': {
+              'box-shadow': '0 25px 50px -12px #18181b'
+            },
+            '100%': {
+              'box-shadow': '0 25px 50px -12px #fefce8'
+            }
+          }
+        }
       }
     },
   },
