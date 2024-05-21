38b3cc829cc491133cb9f0fec1cf8e8038d07186
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-05-20 22:16:37 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/*.{html,js}',
   './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'caveat': ["Caveat", ...defaultTheme.fontFamily.serif]    
+      }
+    },
   },
   plugins: [],
 }
